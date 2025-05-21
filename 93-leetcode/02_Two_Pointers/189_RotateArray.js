816db1a7eafc8ed4220209c20ccce59484147f6f
leontoys <svliyon@gmail.com>
2025-05-22 07:39:21 +1000

    189 leetcode rotate array

---
+++ 93-leetcode/02_Two_Pointers/189_RotateArray.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  k = k % nums.length;
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    let temp = nums[left];
+    nums[left] = nums[right];
+    nums[right] = temp;
+    left++;
+    right--;
+  }
+  left = 0;
+  right = k - 1;
+  while (left < right) {
+    let temp = nums[left];
+    nums[left] = nums[right];
+    nums[right] = temp;
+    left++;
+    right--;
+  }
+  left = k;
+  right = nums.length - 1;
+  while (left < right) {
+    let temp = nums[left];
+    nums[left] = nums[right];
+    nums[right] = temp;
+    left++;
+    right--;
+  }
+};
