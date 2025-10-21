c6c8249964448b4e16cad3fb987ec32858a1ce47
leontoys <svliyon@gmail.com>
2025-10-21 19:47:46 +1100

    selection sort revised

--- 03-grokking-algorithms/02_selectionsort.py
+++ 03-grokking-algorithms/02_selectionsort.py
@@ -1,20 +1,21 @@
 def findSmallest(arr):
-    smallest = arr[0]
     smallest_index = 0
+    smallest = arr[smallest_index]
     for i in range(1,len(arr)):
-        if smallest > arr[i]:
+        if smallest > arr[i] :
             smallest = arr[i]
             smallest_index = i
     return smallest_index
 
 def selectionSort(arr_original):
-    arr_copy = list(arr_original) #to make a copy
+    arr_copy = list(arr_original)
     arr_sorted = []
     while len(arr_copy) > 0:
         smallest_index = findSmallest(arr_copy)
-        arr_sorted.append(arr_copy[smallest_index])
-        arr_copy.pop(smallest_index)
-
+        smallest = arr_copy[smallest_index]
+        arr_sorted.append(smallest)
+        arr_copy.pop(smallest_index)       
     return arr_sorted
+
 
 print(selectionSort([5, 3, 6, 2, 10]))
