49763fbcbb1fe659c398a8e3b8a37ed0dde7c0ad
leontoys <svliyon@gmail.com>
2025-10-21 19:41:20 +1100

    binary search revised

--- 03-grokking-algorithms/01_binarysearch.py
+++ 03-grokking-algorithms/01_binarysearch.py
@@ -1,15 +1,15 @@
 def binary_search(num_list,item):
-    low = 0
-    high = len(num_list) - 1
-    while low <= high:
-        mid = ( low + high ) // 2
+    left = 0
+    right = len(num_list)
+    while left <= right:
+        mid = ( left + right ) // 2
         guess = num_list[mid]
-        if guess == item:
-            return mid 
-        elif guess > item : 
-            high = mid - 1
-        else :
-            low = mid + 1
+        if guess == item : 
+            return mid
+        elif guess > item:
+            right = mid - 1
+        else:
+            left = mid + 1
     return None
 
 print(binary_search([1,3,5,7,9],9))
